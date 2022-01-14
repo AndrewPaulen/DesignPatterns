1202d3e105436e131226ca37f264d738bfc0e628
Andrew Paulen <apaulen@MacBook-Pro-2.local>
2022-01-14 10:49:48 -0500

    Setting up strategy

--- strategy/Driver.java
+++ strategy/Driver.java
@@ -1,9 +1,9 @@
 package strategy;
 
-public class HelloWorld
+public class Driver
 {
     public static void main (String [] args)
     {
-        System.out.println ("Hello World");
+        System.out.println ("Hello");
     }
 }
